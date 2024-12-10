74bdfaad6361fcc0b98cfd1dbafcec0cbdf7b126
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-10 11:38:13 -0300

    feat: add steps e step components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,14 @@
+import { View } from "react-native"
+
+import { Steps } from "@/components/steps"
 import { Welcome } from "@/components/welcome"
-import { Text, View } from "react-native"
 
 export default function Index() {
     return (
         <View style={{ flex: 1, padding: 40, gap: 40 }}>
             <Welcome />
+            <Steps />
+
         </View>
     )
 }
