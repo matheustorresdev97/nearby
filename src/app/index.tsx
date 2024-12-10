804d1c1e8d10fe881f9b5cca3af308b4410fdc03
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-10 11:04:46 -0300

    config: add theme

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native"
 
 export default function Index() {
     return (
-        <View>
+        <View style={{ flex: 1, padding: 40, gap: 40 }}>
             <Text>Hello! Nearby</Text>
         </View>
     )
