d2641d097443c2bf73713619536f3b4bf420b882
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-10 11:09:19 -0300

    config: add font e layout do app

---
+++ src/app/_layout.tsx
@@ -0,0 +1,31 @@
+import { Stack } from "expo-router"
+import { colors } from "@/styles/theme"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+
+import {
+    useFonts,
+    Rubik_600SemiBold,
+    Rubik_400Regular,
+    Rubik_500Medium,
+    Rubik_700Bold,
+} from "@expo-google-fonts/rubik"
+
+export default function Layout() {
+    const [fontsLoaded] = useFonts({
+        Rubik_600SemiBold,
+        Rubik_400Regular,
+        Rubik_500Medium,
+        Rubik_700Bold,
+    })
+
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    contentStyle: { backgroundColor: colors.gray[100] },
+                }}
+            />
+        </GestureHandlerRootView>
+    )
+}
