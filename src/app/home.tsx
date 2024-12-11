5f0da6a7a73d4d1b8d8c6e92dee5127ad7e5a750
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-11 09:53:43 -0300

    feat: add navigation to market screen

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { Alert, Text, View } from "react-native";
 import MapView, { Callout, Marker } from "react-native-maps"
-//import * as Location from "expo-location"
+import * as Location from "expo-location"
 import { router } from "expo-router"
 
 import { api } from "@/services/api"
@@ -52,17 +52,17 @@ export default function Home() {
     }
   }
 
-  //async function getCurrentLocation() {
-  // try {
-  //const { granted } = await Location.requestForegroundPermissionsAsync()
-  //if (granted) {
-  //  const location = await Location.getCurrentPositionAsync()
-  //   console.log(location)
-  //  }
-  // } catch (error) {
-  //  console.log(error)
-  //  }
-  //  }
+  async function getCurrentLocation() {
+    try {
+      const { granted } = await Location.requestForegroundPermissionsAsync()
+      if (granted) {
+        const location = await Location.getCurrentPositionAsync()
+        console.log(location)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 
   useEffect(() => {
